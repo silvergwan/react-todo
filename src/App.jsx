3963ee385b423d84cb61509f64630f9723c03833
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-16 17:10:32 +0900

    retest

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   //test
-  const [count, setCount] = useState(0)
+  //retest
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -30,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
