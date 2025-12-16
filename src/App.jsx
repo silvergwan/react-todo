a41edd10aa79f82344b6878bc4b17d332f3ae5b8
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-16 20:53:33 +0900

    feat: make Counter page( useState example )

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import UserCard from "./components/UserCard";
-import MenuList from "./components/MenuList";
-import Message from "./components/Message";
-import Button from "./components/Button";
-import Card from "./components/Card";
+import Counter from "./pages/Counter";
 
 function App() {
   return (
-    <div>
-      <UserCard name="은관" job="dev" />
-      <MenuList />
-      <Message isLoggedIn={false} />
-      <Button text="확인" />
-      <Card title="일정">
-        <p>전 아이즈원 멤버 김민주 님과의 데이트</p>
-      </Card>
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 
